235ee75f0812eec851fdc90db30ce1153ef371ea
weilyuwang <weilyuwang@gmail.com>
2020-07-12 18:52:24 -0400

    :fire: MongoBD setup

--- blog/blog_server/server.go
+++ blog/blog_server/server.go
@@ -1,16 +1,32 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
 	"net"
 	"os"
 	"os/signal"
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson/primitive"
 
 	"github.com/weilyuwang/grpc-go/blog/blogpb"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 
 	"google.golang.org/grpc"
 )
+
+// declare collection - globally
+var collection *mongo.Collection
+
+type blogItem struct {
+	ID       primitive.ObjectID `bson:"_id,omitempty"` // bson: Binary JSON
+	AuthorID string             `bson:"author_id"`
+	Title    string             `bson:"title"`
+	Content  string             `bson:"content"`
+}
 
 type server struct {
 }
@@ -20,8 +36,27 @@ func main() {
 	// If we crash the go code, we get the file name and line number
 	log.SetFlags(log.LstdFlags | log.Lshortfile)
 
+	// MongoDB Setup
+	// 1. Create a mongo.Client
+	client, err := mongo.NewClient(options.Client().ApplyURI("mongodb://localhost:27017"))
+	if err != nil {
+		log.Fatalf("Error Initializing the Mongo Client: %v\n", err)
+	}
+	// 2. connect it to your running MongoDB server:
+	fmt.Println("Connecting to MongoDB")
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	err = client.Connect(ctx)
+	if err != nil {
+		log.Fatalf("Error connecting to MongoDB Server: %v\n", err)
+	}
+
 	fmt.Println("Blog Service Started")
 
+	// Create/Open up collection
+	collection = client.Database("mydb").Collection("blog")
+
+	// Create Listener on port 500051
 	lis, err := net.Listen("tcp", "0.0.0.0:50051")
 	if err != nil {
 		log.Fatalf("Failed to listen: %v", err)
@@ -49,6 +84,8 @@ func main() {
 	s.Stop()
 	fmt.Println("Closing the listener")
 	lis.Close()
+	fmt.Println("Closing MongoDB Connection")
+	client.Disconnect(ctx)
 	fmt.Println("End of Program")
 
 }
